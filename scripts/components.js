191d52723388d0c04ca66801f49365d848a26781
Callum <callum.rafter@gmail.com>
2022-08-03 17:41:52 +0100

    :heavy_plus_sign: Added header and footers components

---
+++ scripts/components.js
@@ -0,0 +1,60 @@
+// Header component
+class Header extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = `
+        <header>
+            <a href="index" class="nav__home"><h1>CR</h1></a>
+        </header>
+
+        <div class="burger">
+            <span class="burger__menu"></span>
+        </div>
+
+        <nav class="nav">
+            <div class="nav__icons">
+                <a class="nav__icon" href="https://en.wikipedia.org/wiki/Ankh" target="_blank"><img src="./img/svg/ankh.svg" alt="Ankh"></a>
+                <a class="nav__icon" href="https://en.wikipedia.org/wiki/Memento_mori" target="_blank">
+                    <img src="./img/svg/hourglass.svg" alt="Hourglass">
+                    <img src="./img/svg/skull.svg" alt="Skull">
+                </a>
+            </div>
+            <ul class="nav__menu">
+                <li class="nav__item">
+                    <a href="work" class="nav__link link--colour">Work</a>
+                </li>
+                <li class="nav__item">
+                    <a href="books" class="nav__link link--colour">Books</a>
+                </li>
+                <li class="nav__item">
+                    <a href="photos" class="nav__link link--colour">Photos</a>
+                </li>
+            </ul>
+            <div class="nav__icons">
+                <a class="nav__icon" href="https://github.com/callumr00" target="_blank"><img src="./img/svg/github.svg" alt="Github Logo"></a>
+                <a class="nav__icon" href="https://www.linkedin.com/in/callum-rafter" target="_blank"><img src="./img/svg/linkedin.svg" alt="LinkedIn Logo"></a>
+            </div>
+        </nav>
+        `;
+    }
+}
+
+window.customElements.define('header-container', Header);
+
+
+// Footer component
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = `
+        <footer>
+            <div class="footer__icons">        
+                <a class="footer__icon" href="https://github.com/callumr00" target="_blank"><img src="./img/svg/github.svg" alt="Github Logo"></a>
+                <a class="footer__icon" href="https://www.linkedin.com/in/callum-rafter" target="_blank"><img src="./img/svg/linkedin.svg" alt="LinkedIn Logo"></a>
+            </div>
+        </footer>
+        `;
+    }
+}
+
+window.customElements.define('footer-container', Footer);
