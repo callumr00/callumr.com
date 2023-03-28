2fcabeffeb9ac9b0744218e5ec9451d3c58abc6e
callumr00 <callum.rafter@gmail.com>
2023-03-28 08:00:02 +0100

    :heavy_plus_sign: Book Summaries link now opens to /books with Only Book Summaries checked

--- scripts/books.js
+++ scripts/books.js
@@ -9,10 +9,74 @@ tagFilter = ""
 summaryFilter = false
 ratingFilter = 0
 
+// Show filtered results
+updateResults = () => {
+    if (summaryFilter == true) {
+        for (var i=0; i<document.querySelectorAll(".card").length; i++) {
+            cards[i].style.display = ""
+
+            if(
+            tags[i].innerText.toUpperCase().match(tagFilter.toUpperCase()) 
+            && 
+            cards[i].classList.contains("card--link")
+            && 
+            parseInt(ratings[i].innerText) >= ratingFilter) 
+            {
+                cards[i].style.display = ""
+            } else {
+                cards[i].style.display = "none"
+            }
+        }
+    }
+    
+    if (summaryFilter == false) {
+        for (var i=0; i<document.querySelectorAll(".card").length; i++) {
+            cards[i].style.display = ""
+
+            if(
+            tags[i].innerText.toUpperCase().match(tagFilter.toUpperCase()) 
+            && 
+            parseInt(ratings[i].innerText) >= ratingFilter) 
+            {
+                cards[i].style.display = ""
+            } else {
+                cards[i].style.display = "none"
+            }
+        }
+    }
+
+    updateCount()
+}
+
+// Show book count
+updateCount = () => {
+    var array = [].slice.call(cards)
+
+    var displayNone = array.filter(function(el) {
+        return getComputedStyle(el).display === "none"
+    });
+
+    var displayShow = array.filter(function(el) {
+        return getComputedStyle(el).display !== "none"
+    });
+
+    document.querySelector(".count").innerHTML = `${displayShow.length} / ${displayShow.length + displayNone.length}`
+}
+
+updateResults()
+
+// Filter by Tags
 tagsInput.addEventListener('change', (event) => {
     tagFilter = tagsInput.value
     updateResults()
 });
+
+// Only Book Summaries
+if (window.location.href.indexOf("summaries") != -1) {
+    document.getElementById("summaryInput").checked = true;
+    summaryFilter = true
+    updateResults()
+}
 
 summaryInput.addEventListener('change', (event) => {
     if(!summaryInput.checked) {
@@ -24,6 +88,7 @@ summaryInput.addEventListener('change', (event) => {
     }
 });
 
+// Min. Rating
 ratingInput[0].addEventListener('change', (event) => {
     ratingInput[0].checked = true
     ratingInput[1].checked = false
@@ -83,59 +148,3 @@ ratingInput[4].addEventListener('change', (event) => {
 
     updateResults()
 });
-
-// Show filtered results
-updateResults = () => {
-    if (summaryFilter == true) {
-        for (var i=0; i<document.querySelectorAll(".card").length; i++) {
-            cards[i].style.display = ""
-
-            if(
-            tags[i].innerText.toUpperCase().match(tagFilter.toUpperCase()) 
-            && 
-            cards[i].classList.contains("card--link")
-            && 
-            parseInt(ratings[i].innerText) >= ratingFilter) 
-            {
-                cards[i].style.display = ""
-            } else {
-                cards[i].style.display = "none"
-            }
-        }
-    }
-    
-    if (summaryFilter == false) {
-        for (var i=0; i<document.querySelectorAll(".card").length; i++) {
-            cards[i].style.display = ""
-
-            if(
-            tags[i].innerText.toUpperCase().match(tagFilter.toUpperCase()) 
-            && 
-            parseInt(ratings[i].innerText) >= ratingFilter) 
-            {
-                cards[i].style.display = ""
-            } else {
-                cards[i].style.display = "none"
-            }
-        }
-    }
-
-    updateCount()
-}
-
-// Show book count
-updateCount = () => {
-    var array = [].slice.call(cards)
-
-    var displayNone = array.filter(function(el) {
-        return getComputedStyle(el).display === "none"
-    });
-
-    var displayShow = array.filter(function(el) {
-        return getComputedStyle(el).display !== "none"
-    });
-
-    document.querySelector(".count").innerHTML = `${displayShow.length} / ${displayShow.length + displayNone.length}`
-}
-
-updateResults()
