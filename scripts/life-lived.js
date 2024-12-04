f150e00d3c33c0910fc68544ae6f57ffac84aa1e
callumr00 <callum.rafter@protonmail.com>
2024-12-04 21:13:38 +0000

    :hammer: Changed life expectancy value

--- scripts/life-lived.js
+++ scripts/life-lived.js
@@ -5,8 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	ageInMilliseconds = currentDate - birthDate;
 	ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365);
 
-	lifeExpectancyInYears = 82;
+	lifeExpectancyInYears = 80;
 	percentLifeLived = (ageInYears / lifeExpectancyInYears) * 100;
+	percentLifeLived = percentLifeLived.toFixed(2);
 
-	document.getElementById('life-lived').innerHTML = percentLifeLived.toFixed(2);
+	document.getElementById('life-lived').innerHTML = percentLifeLived;
 });
